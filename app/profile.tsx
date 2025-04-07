c4a5c04a72a5b517ea5c42675d95a481eb90d5f9
Huong <huongnguyen.cada@gmail.com>
2025-04-07 17:43:43 -0600

    demo Profile page

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,20 +1,69 @@
-import { Text, View, StyleSheet } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { useRouter} from 'expo-router';
+import { Ionicons } from "@expo/vector-icons"
 
-export default function IdeaScreen() {
+export default function Profile() {
+  const router = useRouter(); // Get the
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Idea</Text>
+      <View style={styles.header}>
+  
+        <Text style={styles.title}> </Text>
+      </View>
+
+      <Image style={styles.profile} source={{uri: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600"}} />
+      
+      <Text>Username: John Doe</Text> 
+{/*
+    <TouchableOpacity style={styles.button} onPress={() => router.push('/lists')}>
+            <Text>Back to Shopping List</Text>
+    </TouchableOpacity>
+*/}
+    <TouchableOpacity style={styles.buttonRed} onPress={() => router.push('/')}>
+            <Text>Logout</Text>  
+    </TouchableOpacity>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#fff',
   },
-  text: {
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+    height: 50,
+    width: '100%',
+  },
+  title: {
     fontSize: 24,
+    fontWeight: 'bold',
+  },
+  profile: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#90EE90',
+    padding: 10,
+    margin: 10,
+    borderRadius: 8,
+  },
+  buttonRed: {
+    backgroundColor: 'red',
+    padding: 10,
+    marginTop: 300,
+    borderRadius: 8,
   },
 });
